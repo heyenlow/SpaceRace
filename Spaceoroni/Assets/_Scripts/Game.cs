e1aa9c4a1d9012ccceb621649795e97f15a62561
heyenlow <57604869+heyenlow@users.noreply.github.com>
2022-02-22 13:53:59 -0600

    UI effects (#24)
    * Added Particles System Dust Effect
    
    Dust Effect on Player1, Builder1.
    -Needs to adjust duration of particle with respect to how long it takes for the object to reach the new position.
    
    * dust
    
    * Insert dust particles behind the astronauts
    
    Co-authored-by: dpoppe7 <70035439+dpoppe7@users.noreply.github.com>

--- Spaceoroni/Assets/_Scripts/Game.cs
+++ Spaceoroni/Assets/_Scripts/Game.cs
@@ -56,7 +56,7 @@ public class Game : MonoBehaviour
     {
         if (isDebug)
         {
-            GameSettings.gameType = GameSettings.GameType.Singleplayer;
+            GameSettings.gameType = GameSettings.GameType.Watch;
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement>().moveCameraToGameBoard();
             StartGame();
         }
@@ -252,7 +252,7 @@ public class Game : MonoBehaviour
     {
         //The Scale Y * 2 of the level object
         const float gamepeiceHeight = (float)0;
-        const float level0Height = (float)0.5001;
+        const float level0Height = (float)0.500;
         const float level4Height = (float)0.000;
 
         float newHeightToMoveTo = 0;
