3a1b6af1ef6ddbb3e3c32c32e2affbfb91c56467
heyenlow <57604869+heyenlow@users.noreply.github.com>
2022-03-16 15:03:49 -0500

    changed the font

--- Spaceoroni/Assets/_Scripts/Listing.cs
+++ Spaceoroni/Assets/_Scripts/Listing.cs
@@ -2,20 +2,21 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Listing : MonoBehaviour
 {
     [SerializeField]
-    private Text _text;
+    private TextMeshProUGUI _text;
 
     public RoomInfo RoomInfo { get; private set; }
 
     public void SetRoomInfo(RoomInfo roomInfo)
     {
         RoomInfo = roomInfo;
-        _text.text = roomInfo.Name + ": " + roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers + " players";
+        _text.text = roomInfo.Name;// + ": " + roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers + " players";
     }
 
     public void OnClick_Button()
