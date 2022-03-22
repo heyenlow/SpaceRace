759d2a397de302f7d288f2ddac2bc4e417b83a9d
heyenlow <57604869+heyenlow@users.noreply.github.com>
2022-03-22 16:12:19 -0500

    Mute Button is synced
    Note: It Only Mutes the Music not sound effect

--- Spaceoroni/Assets/VolumeListener.cs
+++ Spaceoroni/Assets/VolumeListener.cs
@@ -5,8 +5,20 @@ using UnityEngine.UI;
 
 public class VolumeListener : MonoBehaviour
 {
-    public Slider MusicVolumeSliderMain;
-    public Slider MusicVolumeSliderInGame;
+    [SerializeField]
+    private Slider MusicVolumeSliderMain;
+    [SerializeField]
+    private Slider MusicVolumeSliderInGame;
+
+    [SerializeField]
+    private GameObject MuteIG;
+    [SerializeField]
+    private GameObject UnMuteIG;
+    [SerializeField]
+    private GameObject UnMuteMM;
+    [SerializeField]
+    private GameObject MuteMM;
+
 
     public void UpdateVolume(string whichSlider)
     {
@@ -24,5 +36,26 @@ public class VolumeListener : MonoBehaviour
         {
             Debug.LogError("The String passed to update volume is not found: " + whichSlider);
         }
+    }
+
+
+    public void Mute()
+    {
+        GetComponent<AudioSource>().mute = true;
+
+        MuteIG.SetActive(false);
+        MuteMM.SetActive(false);
+        UnMuteIG.SetActive(true);
+        UnMuteMM.SetActive(true);
+    }
+
+    public void UnMute()
+    {
+        GetComponent<AudioSource>().mute = false;
+
+        UnMuteIG.SetActive(false);
+        UnMuteMM.SetActive(false);
+        MuteIG.SetActive(true);
+        MuteMM.SetActive(true);
     }
 }
